fc72d85c9b2068b85047b1fbe66c11522ee064ec
Hasan Al <Hasanalghanim@gmail.com>
2025-08-19 23:29:36 -0600

    Update settings.py

--- CBKC/settings.py
+++ CBKC/settings.py
@@ -122,13 +122,15 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+STATIC_ROOT = '/home/canadianbareknuckle/CBKC/staticfiles'
+
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_ROOT = '/home/canadianbareknuckle/CBKC/media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
