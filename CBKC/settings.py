a8d0a21ded00356e9d03be3a7ca171dc7fe0ab3e
Hasan Al <Hasanalghanim@gmail.com>
2025-09-26 16:09:21 -0600

    Update settings.py

--- CBKC/settings.py
+++ CBKC/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-x%3*2n*2uiv7=0!9dlj$u_-oy^!*c&_0-s=t+sc=bf8i+xu%s^'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['www.canadianbareknuckle.com','canadianbareknuckle.com','127.0.0.1', 'localhost']
 
@@ -106,8 +106,8 @@ WSGI_APPLICATION = 'CBKC.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-        # 'NAME': '/home/canadianbareknuckle/db.sqlite3',
+        # 'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': '/home/canadianbareknuckle/db.sqlite3',
         
     }
 }
